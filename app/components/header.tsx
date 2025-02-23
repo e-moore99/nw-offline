11b9d5c354f91317a565e295e78a57014464cfe4
Evan Moore <evanm@missionreadyhq.com>
2025-02-24 10:07:07 +1300

    working on header + search element

---
+++ app/components/header.tsx
@@ -0,0 +1,26 @@
+import styles from "./header.module.css";
+import Link from "next/link";
+import {
+    ShoppingCartIcon
+  } from '@heroicons/react/24/outline';
+
+export default function Header() {
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.headerTop}>
+          <h1>Welcome to Ecomm test site!</h1>
+            {/* <ShoppingCartIcon className="w-6"/> */}
+            <Link href="/cart">
+            <div>
+
+            <ShoppingCartIcon className="w-6"/>
+            </div></Link> 
+        </div>
+        <div className={styles.headerBottom}>
+          <p>Search element here</p>
+        </div>
+      </div>
+    </>
+  );
+}
