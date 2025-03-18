6de6ba3747513388a97724b9c499fb391c748066
Evan Moore <evanm@missionreadyhq.com>
2025-03-19 10:32:20 +1300

    NWOM-50 disclaimers added to bottom of page under empty trolley button

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -8,7 +8,10 @@ import styles from "./page.module.css";
 import { useDispatch } from "react-redux";
 import { AppDispatch, useAppSelector } from "@/redux/store";
 import { cart, updateCart } from "@/redux/features/cart-slice";
-import { ShoppingBagIcon } from "@heroicons/react/24/outline";
+import {
+  ShoppingBagIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/24/outline";
 
 interface CartItem {
   name: string;
@@ -71,29 +74,47 @@ export default function Cart() {
           <p>! This is an alert banner !</p>
         </div>
         <div className={styles.cartCheckoutBox}>
-        <div className={styles.cartContainer}>
-          {cartItems.length === 0 ? <h1>Cart is empty!</h1> : null}
-          <div className={styles.cartItems}>
-            {cartItems.map((item, index) => (
-              <CartItemCard
-                key={index}
-                name={item.name}
-                id={item.id}
-                image={item.sprites.front_default}
-                quantity={item.quantity}
-                increaseItems={() => increaseItems(index)}
-                decreaseItems={() => decreaseItems(index)}
-                removeItem={() => removeItem(index)}
-              />
-            ))}
+          <div className={styles.cartContainer}>
+            {cartItems.length === 0 ? <h1>Cart is empty!</h1> : null}
+            <div className={styles.cartItems}>
+              {cartItems.map((item, index) => (
+                <CartItemCard
+                  key={index}
+                  name={item.name}
+                  id={item.id}
+                  image={item.sprites.front_default}
+                  quantity={item.quantity}
+                  increaseItems={() => increaseItems(index)}
+                  decreaseItems={() => decreaseItems(index)}
+                  removeItem={() => removeItem(index)}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-        <div className={styles.cartBoxRight}>
-          <div className={styles.orderSummary}>  
-          <h2>Order summary</h2>
+          <div className={styles.cartBoxRight}>
+            <div className={styles.orderSummary}>
+              <h2>Order summary</h2>
+            </div>
+            <button className={styles.emptyTrolley} onClick={emptyCart}>
+              Empty trolley
+            </button>
+            <div className={styles.disclaimer}>
+              <InformationCircleIcon className="w-14" />
+              <p>
+                Some of the items in your cart may have limits. If your order
+                exceeds the advertised limit, you may only receive the limited
+                amount.
+              </p>
+            </div>
+            <div className={styles.disclaimer}>
+              <InformationCircleIcon className="w-20" />
+              <p>
+                Due to the delay between selecting your groceries and delivery,
+                product origins may change before your order is picked and
+                before you receive your products.
+              </p>
+            </div>
           </div>
-          <button className={styles.emptyTrolley} onClick={emptyCart}>Empty trolley</button>
-        </div>
         </div>
       </div>
       <Footer />
