b205dfd5bf07aca1994b5ffff9514c495446cad3
Evan Moore <evanm@missionreadyhq.com>
2025-03-20 11:02:09 +1300

    setting up db and seeding it, setting up db queries

--- app/lib/fetch.ts
+++ app/lib/fetch.ts
@@ -1,4 +1,113 @@
-import { Pokemon } from "./types";
+import { Product, Pokemon } from "./types";
+
+import postgres from 'postgres';
+
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+export async function fetchData() {
+  try {
+    const data = await sql<Product[]>`SELECT * FROM data`;
+    return data;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch revenue data.');
+  }
+}
+
+export async function fetchByName(query: string) {
+  try {
+    const data = await sql<Product[]>`
+      SELECT * FROM data
+      WHERE name = ${`%${query}%`}`;
+
+    const nameFetch = data.map((invoice) => ({
+      ...invoice,
+    }));
+    console.log("fetched by name", nameFetch);
+    return nameFetch;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch the latest invoices.');
+  }
+}
+
+export async function fetchByBrand(query: string) {
+  try {
+    const data = await sql<Product[]>`
+      SELECT * FROM data
+      WHERE brand = ${`%${query}%`}`;
+
+    const brandFetch = data.map((invoice) => ({
+      ...invoice,
+    }));
+    console.log("fetched by name", brandFetch);
+    return brandFetch;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch the latest invoices.');
+  }
+}
+
+const ITEMS_PER_PAGE = 6;
+// export async function fetchFilteredInvoices(
+//   query: string,
+//   currentPage: number,
+// ) {
+//   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+
+//   try {
+//     const invoices = await sql<InvoicesTable[]>`
+//       SELECT
+//         invoices.id,
+//         invoices.amount,
+//         invoices.date,
+//         invoices.status,
+//         customers.name,
+//         customers.email,
+//         customers.image_url
+//       FROM invoices
+//       JOIN customers ON invoices.customer_id = customers.id
+//       WHERE
+//         customers.name ILIKE ${`%${query}%`} OR
+//         customers.email ILIKE ${`%${query}%`} OR
+//         invoices.amount::text ILIKE ${`%${query}%`} OR
+//         invoices.date::text ILIKE ${`%${query}%`} OR
+//         invoices.status ILIKE ${`%${query}%`}
+//       ORDER BY invoices.date DESC
+//       LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
+//     `;
+
+//     return invoices;
+//   } catch (error) {
+//     console.error('Database Error:', error);
+//     throw new Error('Failed to fetch invoices.');
+//   }
+// }
+
+// export async function fetchInvoicesPages(query: string) {
+//   try {
+//     const data = await sql`SELECT COUNT(*)
+//     FROM invoices
+//     JOIN customers ON invoices.customer_id = customers.id
+//     WHERE
+//       customers.name ILIKE ${`%${query}%`} OR
+//       customers.email ILIKE ${`%${query}%`} OR
+//       invoices.amount::text ILIKE ${`%${query}%`} OR
+//       invoices.date::text ILIKE ${`%${query}%`} OR
+//       invoices.status ILIKE ${`%${query}%`}
+//   `;
+
+//     const totalPages = Math.ceil(Number(data[0].count) / ITEMS_PER_PAGE);
+//     return totalPages;
+//   } catch (error) {
+//     console.error('Database Error:', error);
+//     throw new Error('Failed to fetch total number of invoices.');
+//   }
+// }
+
+
+
 
 export const fetchPokemon = async (options: {
   name?: string;
---
+++ app/seed/route.ts
@@ -0,0 +1,60 @@
+import postgres from "postgres";
+import { data } from "../lib/dairy";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+async function seedData() {
+  await sql`
+    CREATE TABLE IF NOT EXISTS data (
+    id VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(255),
+    subtitle VARCHAR(255),
+    brand VARCHAR(100),
+    href VARCHAR(255),
+    availability VARCHAR(255),
+    price_isSaleValueCardDependent BOOLEAN,
+    price_value DECIMAL(10, 2),
+    price_retailValue DECIMAL(10, 2),
+    price_hasLimit BOOLEAN,
+    image_lg VARCHAR(255),
+    image_md VARCHAR(255),
+    image_sm VARCHAR(255),
+    image_xs VARCHAR(255),
+    isRestrictedByAge BOOLEAN,
+    liquorFlag BOOLEAN,
+    isTobacco BOOLEAN,
+    originRegulated BOOLEAN,
+    saleTypes VARCHAR(50),
+    unitOfMeasure VARCHAR(10),
+    unitPrice_plainText VARCHAR(50),
+    unitPrice_value DECIMAL(10, 2),
+    unitPrice_type VARCHAR(10),
+    unitPrice_amount DECIMAL(10, 2),
+    categories VARCHAR(255)
+    );
+  `;
+
+  const insertedData = await Promise.all(
+    data.map(async (data) => {
+      return sql`
+        INSERT INTO data (id, name, subtitle, brand, href, availability, price_isSaleValueCardDependent, price_value, price_retailValue, price_hasLimit, 
+    image_lg, image_md, image_sm, image_xs, isRestrictedByAge, liquorFlag, isTobacco, originRegulated, saleTypes, 
+    unitOfMeasure, unitPrice_plainText, unitPrice_value, unitPrice_type, unitPrice_amount, categories)
+        VALUES (${data.id}, ${data.name}, ${data.subtitle}, ${data.brand}, ${data.href}, ${data.availability}, ${data.price.isSaleValueCardDependent}, ${data.price.value}, ${data.price.retailValue}, ${data.price.hasLimit}, ${data.images.lg}, ${data.images.md}, ${data.images.sm}, ${data.images.xs}, ${data.isRestrictedByAge}, ${data.liquorFlag}, ${data.isTobacco}, ${data.originRegulated}, ${data.saleTypes}, ${data.unitOfMeasure}, ${data.unitPrice.plainText}, ${data.unitPrice.value}, ${data.unitPrice.type}, ${data.unitPrice.amount}, ${data.categories} )
+        ON CONFLICT (id) DO NOTHING;
+      `;
+    })
+  );
+
+  return insertedData;
+}
+
+export async function GET() {
+  try {
+    const result = await sql.begin(() => [seedData()]);
+
+    return Response.json({ message: "Database seeded successfully" });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
