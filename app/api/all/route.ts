8571eef572a7043f208d3de5c66a8a4d2c01bfe1
Evan Moore <evanm@missionreadyhq.com>
2025-03-26 13:27:58 +1300

    more logging added to /api/all route

--- app/api/all/route.ts
+++ app/api/all/route.ts
@@ -2,13 +2,14 @@ import { NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma"; // Adjust path if needed
 
 export async function GET() {
-
+  console.log("API /all called"); // Add this logging
   try {
     const products = await prisma.product.findMany();
     // const count = await prisma.product.count();
+    console.log("All products fetched successfully:", products);
     return NextResponse.json(products, { status: 200 });
   } catch (error) {
-    console.error(error);
+    console.error("Error in fetch all api route",error);
     return NextResponse.json({ error: "An error occurred" }, { status: 500 });
   }
 }
