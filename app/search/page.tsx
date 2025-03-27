3974d6567f4d5a0774d2b5fdcd82654239585aeb
Evan Moore <evanm@missionreadyhq.com>
2025-03-28 09:21:17 +1300

    working on search results page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,17 +1,90 @@
-"use client"
-import React from 'react'
-// import Header from '../components/header'
-import Footer from '../components/Footer'
-// import SearchResults from '../components/SearchResults'
+import React, { useEffect } from "react";
+import { Product } from "../lib/types";
+import { CartItemState } from "@/redux/features/cart-slice";
+import { useDispatch } from "react-redux";
+import { AppDispatch, useAppSelector } from "@/redux/store";
+import { updateCart } from "@/redux/features/cart-slice";
+import ItemCard from "../components/itemCard";
+import styles from "./searchResults.module.css";
 
-const searchPage = () => {
+const SearchPage = ({
+  products,
+  searchQuery,
+}: {
+  products: Product[];
+  searchQuery: string;
+}) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const cartArray = useAppSelector((state) => state.cart);
+
+  const addToCart = (product: Product) => {
+    console.log("added to cart: ", product);
+    const itemIndex = cartArray.findIndex((item) => item.id === product.id);
+
+
+    if (itemIndex !== -1) {
+      const updatedCart = cartArray.map((item, index) =>
+        index === itemIndex ? { ...item, quantity: item.quantity + 1 } : item
+      );
+      dispatch(updateCart(updatedCart));
+    } else {
+      if (!product.name || !product.id) {
+        console.error("Product name or id is undefined");
+        return;
+      }
+      const newCartItem: CartItemState = {
+        name: product.name,
+        id: product.id,
+        images: product.images,
+        quantity: 1,
+        subtitle: product.subtitle,
+        price: product.price,
+        unitPrice: product.unitPrice
+      };
+      const updatedCart = [...cartArray, newCartItem];
+      dispatch(updateCart(updatedCart));
+    }
+  };
+
+  const numberInCart = (id: string) => {
+    const itemIndex = cartArray.findIndex((item) => item.id === id);
+    if (itemIndex !== -1) {
+      return cartArray[itemIndex].quantity;
+    }
+    return 0;
+  };
+
+  useEffect(() => {
+    console.log("Cart array: ", cartArray);
+  }, [cartArray]);
   return (
     <>
-    {/* <Header />
-    <SearchResults /> */}
-    <Footer />
+      <div className={styles.results}>
+        <h1 className={styles.resultsHead}>
+          Search results for &quot;{searchQuery}&quot;
+        </h1>
+        <div className={styles.displayArea}>
+          {products && Array.isArray(products) ? (
+            products.map((product) => (
+              <ItemCard
+              addToCart={() => addToCart(product)}
+              numberInCart={product.id ? numberInCart(product.id) : 0}
+              key={product.id}
+              name={product.name}
+              id={product.id}
+              subtitle={product.subtitle}
+              price={product.price}
+              images={product.images}
+              unitPrice={product.unitPrice}
+              />
+            ))
+          ) : (
+            <p>No results found.</p>
+          )}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default searchPage
+export default SearchPage;
